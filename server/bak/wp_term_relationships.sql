81349da84464e1c2b9931fb1599277b72be32960
x373241884y <x373241884y@gmail.com>
2017-03-05 23:25:03 +0800

    delete bak model js

--- server/bak/wp_term_relationships.sql
+++
@@ -1,9 +0,0 @@
-//MySQL [wp_blog2]> desc wp_term_relationships;
-//+------------------+---------------------+------+-----+---------+-------+
-//| Field            | Type                | Null | Key | Default | Extra |
-//+------------------+---------------------+------+-----+---------+-------+
-//| object_id        | bigint(20) unsigned | NO   | PRI | 0       |       |
-//| term_taxonomy_id | bigint(20) unsigned | NO   | PRI | 0       |       |
-//| term_order       | int(11)             | NO   |     | 0       |       |
-//+------------------+---------------------+------+-----+---------+-------+
-//	3 rows in set (0.00 sec)
--- server/bak/wp_terms.sql
+++
@@ -1,9 +0,0 @@
-MySQL [wp_blog]> describe wp_terms;
-+------------+---------------------+------+-----+---------+----------------+
-| Field      | Type                | Null | Key | Default | Extra          |
-+------------+---------------------+------+-----+---------+----------------+
-| term_id    | bigint(20) unsigned | NO   | PRI | NULL    | auto_increment |
-| name       | varchar(200)        | NO   | MUL |         |                |
-| slug       | varchar(200)        | NO   | MUL |         |                |
-| term_group | bigint(10)          | NO   |     | 0       |                |
-+------------+---------------------+------+-----+---------+----------------+
